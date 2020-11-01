ff93e13b8513dd08b0fcd7d17e5c03fae2a5c0f4
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-11-01 09:13:43 -0300

    Video 2. Make movement smoother by adding time ralation, friction and acceleration

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+const MAX_SPEED = 200
+const ACCELERATION = 50
+const FRICTION = 25
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
@@ -8,9 +12,14 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
+	
+	# Clips the value to a maximum, otherwise diagonals add up
+	input_vector = input_vector.normalized()
+	
+	# multiply by delta makes movement relative to framerate
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_collide(velocity)
