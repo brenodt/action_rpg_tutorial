94530b16cec3c72488295f22570fa52a42e08c5a
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-11-02 13:24:43 -0300

    Add collision to attack animation, set collision layers - video 11

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		# This is a SCENE
+func create_grass_effect():
+	# This is a SCENE
 		var GrassEffect = load("res://Effects/GrassEffect.tscn")
 		# This is a Node <- an INSTANCE of a SCENE
 		var grassEffect = GrassEffect.instance()
@@ -14,8 +13,11 @@ func _process(delta):
 		# we access the `global_position` param of the grass
 		# and set it to the instance.global_position
 		grassEffect.global_position = global_position
-		
-		# This function adds this node to a queue
-		# of nodes that should be destoyed, removed from the game.
-		# The node is, then, removed on the next frame.
-		queue_free()
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	
+	# This function adds this node to a queue
+	# of nodes that should be destoyed, removed from the game.
+	# The node is, then, removed on the next frame.
+	queue_free()
