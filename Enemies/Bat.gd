06f9a411d134d9a9562eff95dcd1040d5e61078e
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-11-03 08:55:07 -0300

    Add inheritance to sword hitbox

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -12,10 +12,8 @@ func _physics_process(delta):
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	stats.health -= 1
-	var knockback_vector = area.knockback_vector
-	knockback = knockback_vector * ACCELERATION
-
+	stats.health -= area.damage
+	knockback = area.knockback_vector * ACCELERATION
 
 func _on_Stats_health_depleted():
 	queue_free()
